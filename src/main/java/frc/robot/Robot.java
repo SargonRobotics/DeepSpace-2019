8d7e8c3822fff10b811721ca7026d73836f4306b
Ubspy <jack-m@live.com>
2019-01-31 13:38:34 -0600

    Removed example files

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,8 +12,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -24,7 +22,7 @@ import frc.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends TimedRobot
 {
-  public static ExampleSubsystem subsystem = new ExampleSubsystem();
+  //public static ExampleSubsystem subsystem = new ExampleSubsystem();
   public static OI oi;
 
   Command autonomousCommand;
@@ -38,7 +36,7 @@ public class Robot extends TimedRobot
   public void robotInit()
   {
     oi = new OI();
-    chooser.setDefaultOption("Default Auto", new ExampleCommand());
+    //chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", chooser);
   }
